835a07add5caa29564876923bff7cd0d876de29c
Polina Kobzareva <polinakobzarevapolina@mail.ru>
2026-07-21 23:23:07 +0300

    e

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import MyUser
 
-admin.site.register(MyUser)
+
+@admin.register(MyUser)
+class MyUserAdmin(UserAdmin):
+    search_fields = ('email', 'username')
