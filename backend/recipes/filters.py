e1bde4a81209be148d7a3db5683946e38f6712f1
Polina Kobzareva <polinakobzarevapolina@mail.ru>
2026-07-21 23:45:01 +0300

    h

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -7,7 +7,9 @@ class RecipeFilter(django_filters.FilterSet):
     author = django_filters.NumberFilter(field_name='author__id')
     tags = django_filters.CharFilter(method='filter_tags')
     is_favorited = django_filters.CharFilter(method='filter_is_favorited')
-    is_in_shopping_cart = django_filters.CharFilter(method='filter_is_in_shopping_cart')
+    is_in_shopping_cart = django_filters.CharFilter(
+        method='filter_is_in_shopping_cart'
+        )
 
     class Meta:
         model = Recipe
